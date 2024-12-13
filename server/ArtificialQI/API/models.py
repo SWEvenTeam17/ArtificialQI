6fdeabf27bb1a4390f4abf3cb640174e5a94e687
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2024-12-13 16:40:44 +0100

    Aggiunti models domanda e risposta

--- server/ArtificialQI/API/models.py
+++ server/ArtificialQI/API/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question = models.TextField()
+    expected_answer = models.TextField()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    LLM_answer = models.TextField()
