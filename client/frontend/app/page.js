0e4dc5336e56d95ef36975fadeb8b9b145dfcab8
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2024-12-23 18:56:13 +0100

    Sistemazioni minori homepage

--- client/frontend/app/page.js
+++ client/frontend/app/page.js
@@ -4,9 +4,26 @@ export default async function Home() {
   const sessions = await response.json();
   return (
     <div className="container">
-      {sessions.map((session) => (
-        <p id={session.id}>{session.name}</p> 
-      ))}
+      <div className="text-center mt-5">
+      <h1 className="fw-light fs-1 fw-bold">ArtificialQi</h1>
+      <p className="fw-light">Seleziona una sessione per cominciare.</p>
+      </div>
+      <div className="row row-cols-2 row-cols-md-4 g-2 mt-5">
+        {sessions.length > 0 ? (
+          sessions.map((session) => (
+            <div className="col">
+              <div className="card h-100 w-100 text-center" key={session.id} id={session.id}>
+                <div className="card-body">
+                  <div className="card-title">{session.title}</div>
+                  <div className="card-text">{session.description}</div>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No sessions available.</p>
+        )}
+      </div>
     </div>
   );
 }
