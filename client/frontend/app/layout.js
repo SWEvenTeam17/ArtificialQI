b24b98bcfcd8e9757b57d491c87319cfad6b420c
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2024-12-25 19:07:54 +0100

    Ricreata home page

--- client/frontend/app/layout.js
+++ client/frontend/app/layout.js
@@ -1,28 +1,108 @@
-
-import Link from 'next/link'
-import Script from 'next/script'
-import './bootstrap.css'
+'use client'
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Form from 'next/form'
+import Script from 'next/script';
+import './bootstrap.css';
 
 export default function RootLayout({ children }) {
+  const [sessions, setSessions] = useState([]);
+  useEffect(() => {
+    fetchSessions();
+  }, []);
+
+  const fetchSessions = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/session_list");
+      const data = await response.json();
+      setSessions(data);
+    } catch (error) {
+      console.error("Error fetching sessions:", error);
+    }
+  };
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = {
+      title: formData.get('title'),
+      description: formData.get('description'),
+    };
+
+    const JSONData = JSON.stringify(data);
+
+    try {
+      await fetch('http://localhost:8000/session_list/', {
+        method: 'POST',
+        headers: { "Content-type": "application/json" },
+        body: JSONData,
+      });
+
+      fetchSessions();
+      event.target.reset();
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  }
+
   return (
     <html lang="en">
       <body>
         <header>
           <Script src="/scripts/bootstrap.bundle.js"></Script>
+          <title>ArtificialQi</title>
         </header>
         <main>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-              <Link className="navbar-brand" href="/">ArtificialQi</Link>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <nav className="navbar bg-body-tertiary fixed-top">
+            <div className="container-fluid justify-content-start">
+              <button className="navbar-toggler me-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" href="/">Home</Link>
-                  </li>
-                </ul>
+              <a className="navbar-brand" href="#">ArtificialQi</a>
+              <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div className="offcanvas-header">
+                  <h1 className="offcanvas-title" id="offcanvasNavbarLabel">ArtificialQi</h1>
+                  <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                  <div className="accordion" id="formAccordion">
+                    <div className="accordion-item">
+                      <h2 className="accordion-header" id="headingForm">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseForm" aria-expanded="true" aria-controls="collapseForm">
+                          Crea nuova sessione
+                        </button>
+                      </h2>
+                      <div id="collapseForm" className="accordion-collapse collapse show" aria-labelledby="headingForm" data-bs-parent="#formAccordion">
+                        <div className="accordion-body">
+                          <div className="container align-items-center">
+                            <div className="mt-3">
+                              <Form onSubmit={onSubmit}>
+                                <div className="form-floating mb-3">
+                                  <input type="text" className="form-control rounded-5" id="title" name="title" placeholder="Titolo" />
+                                  <label htmlFor="title">Titolo</label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                  <input type="text" className="form-control rounded-5" id="description" name="description" placeholder="Descrizione" />
+                                  <label htmlFor="description">Descrizione</label>
+                                </div>
+                                <div className="text-center align-items-center col-12">
+                                  <button type="submit" className="btn btn-primary w-50 rounded-5">Crea</button>
+                                </div>
+                              </Form>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-4">
+                    <ul className="list-group">
+                      {sessions.map((session, index) => (
+                        <a key={index} className='btn mt-2 btn-light rounded-5 border'>{session.title}</a>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </nav>
--- client/frontend/app/page.js
+++ client/frontend/app/page.js
@@ -2,32 +2,5 @@
 export default async function Home() {
   const response = await fetch("http://127.0.0.1:8000/session_list");
   const sessions = await response.json();
-  return (
-    <div className="container">
-      <div className="text-center mt-5">
-        <h1 className="fw-light fs-1 fw-bold">ArtificialQi</h1>
-        <p className="fw-light">Seleziona una sessione per cominciare.</p>
-      </div>
-      <div className="row row-cols-2 row-cols-md-4 g-2 mt-5">
-        {sessions.map((session) => (
-          <div key={session.id} className="col">
-            <a className="card text-bg-light hover-grow text-decoration-none rounded-5 shadow h-100 w-100 text-center"  id={session.id}>
-              <div className="card-body">
-                <div className="card-title">{session.title}</div>
-                <div className="card-text">{session.description}</div>
-              </div>
-            </a>
-          </div>
-        ))
-        }
-        <div className="col d-flex justify-content-center align-items-center">
-          <a className="card border-primary text-bg-light hover-grow text-decoration-none align-items-center rounded-5 shadow w-100 text-center">
-            <div className="card-body">
-              <div className="card-title">Aggiungi sessione</div>
-            </div>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+  return (<div className="container"></div> );
 }
