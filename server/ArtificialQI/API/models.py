cba8bc183cd69f5f9b1f19d8bf7d14384ae7d2c8
Gyarik <alex64.AT93@gmail.com>
2025-01-07 18:32:21 +0100

    add N:N relationship between Session and LLM

--- server/ArtificialQI/API/models.py
+++ server/ArtificialQI/API/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 
+class LLM(models.Model):
+    name = models.TextField()
+    n_parameters = models.TextField(default="")
+    # session = models.ForeignKey(Session, on_delete=models.CASCADE)
+
 
 class Session(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(default="")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    llm = models.ManyToManyField(LLM)
 
 
 class Prompt(models.Model):
     prompt_text = models.TextField()
     expected_answer = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    session = models.ForeignKey(Session, on_delete=models.CASCADE)
-
-
-class LLM(models.Model):
-    name = models.TextField()
-    n_parameters = models.TextField(default="")
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
 
 
