d2b945bfaf66f82cdf657c25e9ea3066b0e49abc
Gyarik <alex64.AT93@gmail.com>
2025-01-23 21:42:51 +0100

    componentize QnAForm

---
+++ client/frontend/app/components/prompt/QnAForm.js
@@ -0,0 +1,81 @@
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useResponse } from "../contexts/ResponseContext";
+import Form from "next/form";
+
+const QnAForm = ({ sessionData }) => {
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    const [question, setQuestion] = useState("");
+    const [answer, setAnswer] = useState("");
+    const { setResponseData } = useResponse();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        try {
+            const response = await fetch("http://localhost:8000/runtest", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ question: question, answer: answer, sessionId: sessionData.id }),
+            });
+
+            const data = await response.json();
+            console.log(data);
+            setResponseData(data.responses);
+            router.push(`/sessions/${sessionData.id}/results`);
+
+        } catch (error) {
+            console.error("Error submitting form:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <div className="form-floating mb-3">
+                <input
+                    type="text"
+                    className="form-control rounded-5"
+                    id="question"
+                    name="question"
+                    placeholder="Domanda"
+                    value={question}
+                    onChange={(e) => setQuestion(e.target.value)}
+                />
+                <label htmlFor="question">Domanda</label>
+            </div>
+            <div className="form-floating mb-3">
+                <input
+                    type="text"
+                    className="form-control rounded-5"
+                    id="answer"
+                    name="answer"
+                    placeholder="Risposta attesa"
+                    value={answer}
+                    onChange={(e) => setAnswer(e.target.value)}
+                />
+                <label htmlFor="answer">Risposta attesa</label>
+            </div>
+            <div className="text-center align-items-center col-12">
+                <button
+                    type="submit"
+                    className="btn btn-primary w-50 rounded-5"
+                    disabled={loading}
+                >
+                    {loading ? (
+                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                    ) : (
+                        "Invia"
+                    )}
+                </button>
+            </div>
+        </Form>
+    );
+}
+
+export default QnAForm;
--- client/frontend/app/sessions/[id]/page.js
+++ client/frontend/app/sessions/[id]/page.js
@@ -1,22 +1,15 @@
 'use client'
 import { use, useState, useEffect, useContext } from "react";
 import { SessionContext } from "@/app/components/contexts/SessionContext";
-import { useResponse } from "@/app/components/contexts/ResponseContext";
-import { useRouter } from "next/navigation";
-import Form from 'next/form';
 import AddLLMForm from "@/app/components/LLM/AddLLMForm";
 import LLMCard from "@/app/components/LLM/LLMCard";
+import QnAForm from "@/app/components/prompt/QnAForm";
 
 export default function SessionPage({ params }) {
-    const router = useRouter();
     const { id } = use(params);
     const sessions = useContext(SessionContext);
     const [sessionData, setSessionData] = useState(null);
     const [LLMData, setLLMData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [question, setQuestion] = useState("");
-    const [answer, setAnswer] = useState("");
-    const { setResponseData } = useResponse();
 
     useEffect(() => {
         fetchSessionData();
@@ -25,31 +18,6 @@ export default function SessionPage({ params }) {
     useEffect(() => {
         fetchLLMData();
     }, []);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setLoading(true);
-
-        try {
-            const response = await fetch("http://localhost:8000/runtest", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ question: question, answer: answer, sessionId: sessionData.id }),
-            });
-
-            const data = await response.json();
-            console.log(data);
-            setResponseData(data.responses);
-            router.push(`/sessions/${id}/results`);
-
-        } catch (error) {
-            console.error("Error submitting form:", error);
-        } finally {
-            setLoading(false);
-        }
-    };
 
     const submitLLM = async (e) => {
         e.preventDefault();
@@ -136,45 +104,7 @@ export default function SessionPage({ params }) {
                 </div>
             )}
             <div className="text-center justify-content-center">
-                <Form onSubmit={handleSubmit}>
-                    <div className="form-floating mb-3">
-                        <input
-                            type="text"
-                            className="form-control rounded-5"
-                            id="question"
-                            name="question"
-                            placeholder="Domanda"
-                            value={question}
-                            onChange={(e) => setQuestion(e.target.value)}
-                        />
-                        <label htmlFor="question">Domanda</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input
-                            type="text"
-                            className="form-control rounded-5"
-                            id="answer"
-                            name="answer"
-                            placeholder="Risposta attesa"
-                            value={answer}
-                            onChange={(e) => setAnswer(e.target.value)}
-                        />
-                        <label htmlFor="answer">Risposta attesa</label>
-                    </div>
-                    <div className="text-center align-items-center col-12">
-                        <button
-                            type="submit"
-                            className="btn btn-primary w-50 rounded-5"
-                            disabled={loading}
-                        >
-                            {loading ? (
-                                <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                            ) : (
-                                "Invia"
-                            )}
-                        </button>
-                    </div>
-                </Form>
+                <QnAForm sessionData={sessionData} />
             </div>
         </div>
     );
