9fb0c341d31d374a65bd8677d2b16b446829b52b
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2025-01-27 19:22:54 +0100

    Cambiata porta

--- server/ArtificialQI/API/classes/LLMController.py
+++ server/ArtificialQI/API/classes/LLMController.py
@@ -2,7 +2,7 @@ from langchain_ollama import OllamaLLM
 
 class LLMController:
     def __init__(self, LLMName, host="localhost", port=8000):
-        base_url = "http://ollama:5000"
+        base_url = "http://ollama:11434"
         self.llm = OllamaLLM(model=LLMName, base_url=base_url)
 
 
