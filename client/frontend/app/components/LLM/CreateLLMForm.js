0601d655600cf2b183c51242217d65560d5ef8d2
Gyarik <alex64.AT93@gmail.com>
2025-02-19 14:32:26 +0100

    add CreateLLMForm validation

--- client/frontend/app/components/LLM/CreateLLMForm.js
+++ client/frontend/app/components/LLM/CreateLLMForm.js
@@ -1,9 +1,34 @@
 import Form from "next/form";
+import { useState } from "react";
 
 const CreateLLMForm = ({ fetchLLMList }) => {
+    const [formErrors, setFormErrors] = useState({});
+    const [name, setName] = useState("");
+    const [parameters, setParameters] = useState("");
+
+    const validateForm = () => {
+        const errors = {};
+
+        if (!name) {
+            errors.name = "Il nome è obbligatorio.";
+        }
+
+        if (!parameters) {
+            errors.n_parameters = "Il numero di parametri è obbligatorio.";
+        }
+
+        return errors;
+    };
 
     const createLLM = async (e) => {
         e.preventDefault();
+        const errors = validateForm();
+        if(Object.keys(errors).length > 0) {
+            setFormErrors(errors);
+            return;
+        }
+
+        setFormErrors({});
 
         const formData = new FormData(e.target);
         const data = {
@@ -38,20 +63,26 @@ const CreateLLMForm = ({ fetchLLMList }) => {
                         type="text"
                         id="name"
                         name="name"
-                        className="form-control mt-3 rounded-5"
+                        className={`form-control mt-3 rounded-5 ${formErrors.name ? "is-invalid" : ""}`}
                         placeholder="Nome"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                     />
                     <label htmlFor="name">Nome</label>
+                    {formErrors.name && <div className="invalid-feedback">{formErrors.name}</div>}
                 </div>
                 <div className="form-floating mb-3">
                     <input
                         type="text"
                         id="nparameters"
                         name="nparameters"
-                        className="form-control mt-3 rounded-5"
+                        className={`form-control mt-3 rounded-5 ${formErrors.n_parameters ? "is-invalid" : ""}`}
                         placeholder="Numero Parametri"
+                        value={parameters}
+                        onChange={(e) => setParameters(e.target.value)}
                     />
                     <label htmlFor="description">Numero Parametri</label>
+                    {formErrors.n_parameters && <div className="invalid-feedback">{formErrors.n_parameters}</div>}
                 </div>
                 <div className="text-center align-items-center col-12">
                     <button
