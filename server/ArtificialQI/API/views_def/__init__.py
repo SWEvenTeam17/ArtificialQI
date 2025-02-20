f3b3db38c57e158b9a951e4ec948edc831225c82
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2025-02-20 01:26:49 +0100

    Terminata analisi statica con valutazione 91.9%

--- server/ArtificialQI/API/views_def/__init__.py
+++ server/ArtificialQI/API/views_def/__init__.py
@@ -1,3 +1,8 @@
+"""
+Modulo che contiene tutte le definizioni delle viste necessarie
+per la gestione della logica delle operazioni eseguite
+dal server
+"""
 from .answer_views import *
 from .llm_views import *
 from .prompt_views import *
