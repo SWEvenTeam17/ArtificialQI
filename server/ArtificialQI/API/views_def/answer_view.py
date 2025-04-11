4c1aec69a611de44880fc35a27d4698ab9138d3a
Gyarik <alex64.AT93@gmail.com>
2025-04-11 18:15:57 +0200

    add most class-based views

---
+++ server/ArtificialQI/API/views_def/answer_view.py
@@ -0,0 +1,7 @@
+from .abstract_view import AbstractView
+from API.serializers import AnswerSerializer
+from API.services import AnswerService
+
+class AnswerView(AbstractView):
+    serializer = AnswerSerializer
+    service = AnswerService
