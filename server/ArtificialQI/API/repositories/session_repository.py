269711580c05795157ecaaf2e98565457dc149dd
Gyarik <alex64.AT93@gmail.com>
2025-04-14 13:42:31 +0200

    add SessionLLMView

--- server/ArtificialQI/API/repositories/session_repository.py
+++ server/ArtificialQI/API/repositories/session_repository.py
@@ -1,6 +1,19 @@
-from API.models import Session
-from API.serializers import SessionSerializer
+from API.models import Session, LLM
 from .abstract_repository import AbstractRepository
 
 class SessionRepository(AbstractRepository):
     model = Session
+
+    @classmethod
+    def get_llm(cls, session_id: int):
+        return LLM.objects.exclude(session__id=session_id).all()
+
+    @classmethod
+    def add_llm(cls, session: Session, llm: LLM):
+        session.llm.add(llm)
+        session.save()
+        return llm
+    
+    @classmethod
+    def delete_llm(cls, session: Session, llm: LLM):
+        session.llm.remove(llm)
--- server/ArtificialQI/API/urls.py
+++ server/ArtificialQI/API/urls.py
@@ -4,6 +4,7 @@ from .views_def.sessions_view import SessionsView
 from .views_def.prompt_view import PromptView
 from .views_def.llm_view import LLMView, OllamaView
 from .views_def.answer_view import AnswerView
+from .views_def.session_llm_view import SessionLLMView
 
 urlpatterns = [
     path("session_list/", SessionsView.as_view()),
@@ -16,8 +17,8 @@ urlpatterns = [
     path("llm_list/", LLMView.as_view()),
     path("llm_list/<int:pk>/", LLMView.as_view()),
     path("llm_list/load_ollama/", OllamaView.as_view()),
-    path("llm_add/", views.add_llm_session),
-    path("llm_remaining/<int:pk>", views.get_llm_session),
-    path("llm_delete/<int:session_id>/<int:llm_id>", views.delete_llm_session),
+    path("llm_add/", SessionLLMView.as_view()),
+    path("llm_remaining/<int:pk>", SessionLLMView.as_view()),
+    path("llm_delete/<int:session_id>/<int:llm_id>", SessionLLMView.as_view()),
     path("runtest", views.runtest),
 ]
