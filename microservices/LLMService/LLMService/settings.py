8d7d040808296a43529d5452e01ff62229a61160
Matteo Mazzotti <matteo.mazzotti@outlook.it>
2025-04-17 17:20:55 +0200

    Terminata separazione modulo interrogazione LLM

--- microservices/LLMService/LLMService/settings.py
+++ microservices/LLMService/LLMService/settings.py
@@ -76,3 +76,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'UNAUTHENTICATED_USER': None,
+}
--- microservices/LLMService/api/views.py
+++ microservices/LLMService/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from api.services import LLMService
 
-# Create your views here.
+class LLMView(APIView):
+
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        pass
+    def post(self, request):
+        llm_name = request.data.get("llm_name")
+        prompt = request.data.get("prompt")
+        answer = LLMService.interrogate(llm=LLMService.get_llm(llm_name), prompt=prompt)
+        return Response({"llm_name":llm_name,"prompt":prompt,"answer":answer})
+
+        
