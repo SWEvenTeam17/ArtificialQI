579f14d26cdf48bc02b0d08943eed34915c910ec
Matteo Mazzotti <matteo.mazzotti@outlook.it>
2025-04-18 16:54:32 +0200

    Fix minori

--- server/ArtificialQI/API/repositories/llm_repository.py
+++ server/ArtificialQI/API/repositories/llm_repository.py
@@ -4,3 +4,11 @@ from .abstract_repository import AbstractRepository
 
 class LLMRepository(AbstractRepository):
     model = LLM
+
+    @staticmethod 
+    def update_or_create(name: str, parameters: str)->bool:
+        llm, exists = LLM.objects.get_or_create(name=name)
+        if not exists or llm.n_parameters != parameters:
+            llm.n_parameters = parameters
+            llm.save()
+        return True
