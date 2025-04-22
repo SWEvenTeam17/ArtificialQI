db029929d8337a6b34f7f9645301058f33b20921
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2025-04-22 16:21:49 +0200

    Refactoring di alcune funzioni errate per posizionamento

--- server/ArtificialQI/API/views_def/prev_test_view.py
+++ server/ArtificialQI/API/views_def/prev_test_view.py
@@ -1,6 +1,6 @@
 from .abstract_view import AbstractView
 from API.serializers import PromptSerializer
-from API.services import TestService
+from API.services import TestService, SessionService, PromptService
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -11,7 +11,9 @@ class PrevTestView(AbstractView):
 
     def get(self, request, pk):
         try:
-            previous_prompts = self.service.get_prev(pk)
+            previous_prompts = PromptService.filter_by_session(
+                session=SessionService.read(id=pk)
+            )
             serializer = self.serializer(previous_prompts, many=True)
             return Response(serializer.data)
         except Exception as e:
@@ -19,7 +21,7 @@ class PrevTestView(AbstractView):
 
     def delete(self, request, pk):
         try:
-            self.service.delete_prompt(request)
+            PromptService.delete(id=pk)
             return Response(status.HTTP_204_NO_CONTENT)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
