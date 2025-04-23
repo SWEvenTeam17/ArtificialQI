412c3386f3d951c2b2a943c32bc5ea169286ac57
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2025-04-23 23:38:59 +0200

    Sistemazioni ulteriori alla qualità del codice

--- server/ArtificialQI/API/views_def/answer_view.py
+++ server/ArtificialQI/API/views_def/answer_view.py
@@ -1,8 +1,13 @@
-from .abstract_view import AbstractView
+"""
+File che contiene le definizioni della vista AnswerView.
+"""
 from API.serializers import AnswerSerializer
 from API.services import AnswerService
-
+from .abstract_view import AbstractView
 
 class AnswerView(AbstractView):
+    """
+    Classe che definisce le operazioni disponibili per le risposte.
+    """
     serializer = AnswerSerializer
     service = AnswerService
