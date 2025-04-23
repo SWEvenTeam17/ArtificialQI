f232e232266f81c9e5236ccb6ef425c0fa6ea414
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2025-04-23 15:29:31 +0200

    Continuato miglioramento codice.

--- server/ArtificialQI/API/views_def/session_llm_view.py
+++ server/ArtificialQI/API/views_def/session_llm_view.py
@@ -1,15 +1,25 @@
-from .abstract_view import AbstractView
-from API.serializers import LLMSerializer
-from API.services import SessionService
+"""
+File che contiene la classe SessionLLMView.
+"""
+
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+from API.serializers import LLMSerializer
+from API.services import SessionService
 
-
-class SessionLLMView(AbstractView):
+class SessionLLMView(APIView):
+    """
+    Classe che contiene la definizione della vista dedicata
+    alla gestione della connessione sessione-llm.
+    """
     serializer = LLMSerializer
     service = SessionService
 
-    def get(self, request, pk: int):
+    def get(self, request, pk: int)->Response:
+        """
+        Ritorna tutti i modelli collegati ad una sessione.
+        """
         try:
             result = self.service.get_llm(session_id=pk)
             serializer = self.serializer(result, many=True)
@@ -17,7 +27,10 @@ class SessionLLMView(AbstractView):
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
 
-    def post(self, request):
+    def post(self, request)->Response:
+        """
+        Collega un LLM ad una sessione.
+        """
         try:
             session_id = request.data.get("sessionId")
             llm_id = request.data.get("llmId")
@@ -28,7 +41,10 @@ class SessionLLMView(AbstractView):
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
 
-    def delete(self, request, session_id: int, llm_id: int):
+    def delete(self, request, session_id: int, llm_id: int)->Response:
+        """
+        Rimuove un LLM da una sessione.
+        """
         try:
             self.service.delete_llm(session_id, llm_id)
             return Response(status=status.HTTP_204_NO_CONTENT)
