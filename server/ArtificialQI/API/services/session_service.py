fb4d15bf42c516b9261d379ffd03f26877a42fbb
Gyarik <alex64.AT93@gmail.com>
2025-04-25 14:49:17 +0200

    rework error handling

--- server/ArtificialQI/API/services/session_service.py
+++ server/ArtificialQI/API/services/session_service.py
@@ -3,7 +3,7 @@ File che contiene i servizi riguardanti le sessioni.
 """
 
 from API.repositories import SessionRepository, LLMRepository
-from API.models import Session, LLM
+from API.models import LLM
 from .abstract_service import AbstractService
 
 
@@ -19,37 +19,22 @@ class SessionService(AbstractService):
         """
         Funzione che ritorna tutti i LLM connessi ad una sessione.
         """
-        try:
-            return cls.repository.get_llm(session_id=session_id)
-        except Session.DoesNotExist:
-            return "Session not found"
-        except LLM.DoesNotExist:
-            return "LLM not found"
+        return cls.repository.get_llm(session_id=session_id)
 
     @classmethod
     def add_llm(cls, session_id: int, llm_id: int) -> LLM | str:
         """
         Funzione che aggiunge un LLM ad una sessione.
         """
-        try:
-            session = cls.repository.get_by_id(session_id)
-            llm = LLMRepository.get_by_id(llm_id)
-            return cls.repository.add_llm(session, llm)
-        except Session.DoesNotExist:
-            return "Session not found"
-        except LLM.DoesNotExist:
-            return "LLM not found"
+        session = cls.repository.get_by_id(session_id)
+        llm = LLMRepository.get_by_id(llm_id)
+        return cls.repository.add_llm(session, llm)
 
     @classmethod
     def delete_llm(cls, session_id: int, llm_id: int) -> None | str:
         """
         Funzione che rimuove un LLM da una sessione.
         """
-        try:
-            session = cls.repository.get_by_id(session_id)
-            llm = LLMRepository.get_by_id(llm_id)
-            return cls.repository.delete_llm(session, llm)
-        except Session.DoesNotExist:
-            return "Session not found"
-        except LLM.DoesNotExist:
-            return "LLM not found"
+        session = cls.repository.get_by_id(session_id)
+        llm = LLMRepository.get_by_id(llm_id)
+        return cls.repository.delete_llm(session, llm)
--- server/ArtificialQI/API/views_def/session_llm_view.py
+++ server/ArtificialQI/API/views_def/session_llm_view.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from API.serializers import LLMSerializer
 from API.services import SessionService
+from API.models import Session, LLM
 
 class SessionLLMView(APIView):
     """
@@ -24,8 +25,12 @@ class SessionLLMView(APIView):
             result = self.service.get_llm(session_id=instance_id)
             serializer = self.serializer(result, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
+        except Session.DoesNotExist:
+            return Response({"error": "Session not found"}, status=status.HTTP_404_NOT_FOUND)
+        except LLM.DoesNotExist:
+            return Response({"error": "LLM not found"}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def post(self, request)->Response:
         """
@@ -38,8 +43,12 @@ class SessionLLMView(APIView):
                 self.service.add_llm(session_id=session_id, llm_id=llm_id)
             )
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Session.DoesNotExist:
+            return Response({"error": "Session not found"}, status=status.HTTP_404_NOT_FOUND)
+        except LLM.DoesNotExist:
+            return Response({"error": "LLM not found"}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def delete(self, request, session_id: int, llm_id: int)->Response:
         """
@@ -48,5 +57,9 @@ class SessionLLMView(APIView):
         try:
             self.service.delete_llm(session_id, llm_id)
             return Response(status=status.HTTP_204_NO_CONTENT)
+        except Session.DoesNotExist:
+            return Response({"error": "Session not found"}, status=status.HTTP_404_NOT_FOUND)
+        except LLM.DoesNotExist:
+            return Response({"error": "LLM not found"}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
