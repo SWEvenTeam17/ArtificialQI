e1724a47e3a7771dbfe7aefa6ac4b234df900f19
Valentina Schivo <valentina.schivo@studenti.unipd.it>
2025-05-02 17:42:04 +0200

    test per answer_service

--- server/ArtificialQI/API/tests/test_llm_controller.py
+++ server/ArtificialQI/API/tests/test_llm_controller.py
@@ -15,7 +15,7 @@ class LLMControllerTests(TestCase):
         score = llm.get_semantic_evaluation("Hello", "Goodbye")
         self.assertTrue(0 <= score < 50) 
   
-  "patch simula chiamate API esterne"
+  #patch simula chiamate API esterne
     @patch("requests.get")  # Mock della chiamata API a Ollama
     def test_get_answer_from_llm(self, mock_get):
         """Testa che get_answer() restituisca una risposta valida."""
