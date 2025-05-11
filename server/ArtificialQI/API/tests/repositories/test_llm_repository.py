533ae2e6c74e989e1f13aed1ca8df2fc99ba18ed
alessiobarraco <101570067+alessiobarraco@users.noreply.github.com>
2025-05-11 19:09:01 +0200

    test repositories

---
+++ server/ArtificialQI/API/tests/repositories/test_llm_repository.py
@@ -0,0 +1,36 @@
+from API.models import Answer, Prompt, LLM, Session
+from API.repositories.llm_repository import LLMRepository
+from API.tests.repositories.abstract_repository import AbstractRepository
+from django.test import TestCase
+
+class LLMRepositoryTest(AbstractRepository, TestCase):
+
+      #def setUp(self):
+        #self.llm = LLM.objects.create(name="llama3.2", n_parameters="3B")
+
+    @property
+    def repository(self):
+        return LLMRepository
+
+    @property
+    def valid_data(self):
+        return {
+            "name": "llama3.2",
+            "n_parameters": "3B"
+        }
+
+    def test_update_or_create(self):
+        # test: aggiungere modello non esistente
+        self.repository.update_or_create("mod1", "3B")
+        results = self.repository.get_all()
+        self.assertEqual(len(results),1)
+        self.assertEqual(self.repository.get_by_id(1).name, "mod1")
+        self.assertEqual(self.repository.get_by_id(1).n_parameters, "3B")
+        # test: aggiornare modello
+        self.repository.update_or_create("mod1", "6B")
+        self.assertEqual(self.repository.get_by_id(1).name, "mod1")
+        self.assertEqual(self.repository.get_by_id(1).n_parameters, "6B")
+        # test: aggiornare modello con gli stessi dati
+        self.repository.update_or_create("mod1", "6B")
+        self.assertEqual(self.repository.get_by_id(1).name, "mod1")
+        self.assertEqual(self.repository.get_by_id(1).n_parameters, "6B")
