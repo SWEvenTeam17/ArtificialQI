44aa62dcdc36e1b9b9fe8f888929abaadb54b4a9
alessiobarraco <101570067+alessiobarraco@users.noreply.github.com>
2025-05-21 23:31:20 +0200

    merge

--- server/ArtificialQI/API/urls.py
+++ server/ArtificialQI/API/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views_def.sessions_view import SessionsView
 from .views_def.prompt_view import PromptView
-from .views_def.llm_view import LLMView, OllamaView, LLMComparisonView, PromptComparisonView
+from .views_def.llm_view import LLMView, OllamaView
 from .views_def.session_llm_view import SessionLLMView
 from .views_def.test_view import TestView
 from .views_def.prev_test_view import PrevTestView
 from .views_def.block_view import BlockView
 from .views_def.run_view import RunPromptView
 from .views_def.block_view import BlockView
-from .views_def.block_view import BlockView
+from .views_def.run_view import RunPromptView
 
 urlpatterns = [
     path("session_list/", SessionsView.as_view()),
