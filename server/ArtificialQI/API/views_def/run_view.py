dbdbc0216de299e9e4287abb2a4e941ef40d6726
alessiobarraco <101570067+alessiobarraco@users.noreply.github.com>
2025-05-21 23:17:15 +0200

    Revert "commit urls e run_view prova"
    This reverts commit 9f739284a7619595e12cf498c060afa2e24ede89.

--- server/ArtificialQI/API/views_def/run_view.py
+++ server/ArtificialQI/API/views_def/run_view.py
@@ -1,11 +1,14 @@
+"""
+File che contiene la classe PromptView.
+"""
+
 from API.serializers import RunSerializer
 from API.services import RunService
 from .abstract_view import AbstractView
 
-class RunView(AbstractView):
+class PromptView(AbstractView):
     """
-    Classe che contiene le definizioni delle viste dedicate alla gestione delle run.
+    Classe che contiene le definizioni delle viste dedicate alla gestione dei Prompt.
     """
-
     serializer = RunSerializer
     service = RunService
