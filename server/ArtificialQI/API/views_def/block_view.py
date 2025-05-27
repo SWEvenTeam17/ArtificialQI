0ccc82f1095771afd5c8fd314e856e5e81952c75
Matteo Mazzotti <matteo.mazzotti@outlook.it>
2025-05-27 17:35:02 +0200

    Risolto bug cancellazione blocco appena creato

--- server/ArtificialQI/API/views_def/block_view.py
+++ server/ArtificialQI/API/views_def/block_view.py
@@ -40,7 +40,8 @@ class BlockView(AbstractView):
                     {"error": "Nome duplicato"},
                     status=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 )
-            return Response(data, status=status.HTTP_201_CREATED)
+            serialized = self.serializer(result)
+            return Response(serialized.data, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
