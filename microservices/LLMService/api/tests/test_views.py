f9b780c796085e7dbd691cc9b6fc9c8dc3bd4b64
Matteo Mazzotti <matteo.mazzotti@outlook.it>
2025-06-02 16:44:54 +0200

    Ripristinati test LLMService

--- microservices/LLMService/api/tests/test_views.py
+++ microservices/LLMService/api/tests/test_views.py
@@ -16,7 +16,7 @@ def factory():
 # --- Test GET ---
 
 
-@patch("api.views.LLMService.get_ollama_llms")
+@patch("api.services.OllamaLLMIntegrationService.get_ollama_llms")
 def test_llmserviceview_get_success(mock_get_ollama_llms, factory):
     mock_get_ollama_llms.return_value = [{"name": "llm1"}]
     request = factory.get("/llms/")
@@ -26,7 +26,7 @@ def test_llmserviceview_get_success(mock_get_ollama_llms, factory):
     assert response.data == [{"name": "llm1"}]
 
 
-@patch("api.views.LLMService.get_ollama_llms", return_value=None)
+@patch("api.services.OllamaLLMIntegrationService.get_ollama_llms", return_value=None)
 def test_llmserviceview_get_failure(mock_get_ollama_llms, factory):
     request = factory.get("/llms/")
     view = LLMServiceView.as_view()
@@ -38,8 +38,8 @@ def test_llmserviceview_get_failure(mock_get_ollama_llms, factory):
 # --- Test POST ---
 
 
-@patch("api.views.LLMService.interrogate")
-@patch("api.views.LLMService.get_llm")
+@patch("api.services.OllamaLLMIntegrationService.interrogate")
+@patch("api.services.OllamaLLMIntegrationService.get_llm")
 def test_llmserviceview_post_success(mock_get_llm, mock_interrogate, factory):
     mock_get_llm.return_value = MagicMock()
     mock_interrogate.return_value = "Risposta"
@@ -53,7 +53,7 @@ def test_llmserviceview_post_success(mock_get_llm, mock_interrogate, factory):
     assert response.data["answer"] == "Risposta"
 
 
-@patch("api.views.LLMService.get_llm", return_value=None)
+@patch("api.services.OllamaLLMIntegrationService.get_llm", return_value=None)
 def test_llmserviceview_post_failure(mock_get_llm, factory):
     data = {"llm_name": "llm1", "prompt": "ciao"}
     request = factory.post("/llms/", data, format="json")
